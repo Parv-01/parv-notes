5f99524ccf6daaaf9e231befc3d89ef7eaeb3b13
PArv Agarwal <parvaggarwal9759@gmail.com>
2025-07-08 05:00:19 +0530

    learned about for,switch,goto,break,continue & scope in golang

--- Go_Notes_Nd_Ex/goFlow.go
+++ Go_Notes_Nd_Ex/goFlow.go
@@ -2,23 +2,41 @@ package main
 
 import (
 	"fmt"
-	//"os"
-	"strconv"
 )
+import f "fmt" //permitted and used to learn about scopes 
+
+const done =false //package type scope
+var a =5 //will not return error even if not used :- package scope 
 
 func main(){
 	fmt.Println("Ich leibst koding.")
-	//If Else and else if conditions
+	/*If Else and else if conditions*/
 	//ifElse()
 
-	//Command Line arguments{User input} use os.Args
+	/*Command Line arguments{User input} use os.Args*/
 	//cmdlinearg()
 
-	//Simple If or short way of writing if statements
+	/*Simple If or short way of writing if statements*/
 	// simpleIf()
 
-	//For Loop and loops
-	forLoop()
+	/*For Loop and loops*/
+	// forLoop()
+
+	/*Continue  and Break Statement */
+	// continueBreakstat()
+
+	/*Label Statements */
+	// labelstat()
+
+	/*goto and switch statements*/
+	//gotoSwitchstat()
+
+	/*Scope in Golang*/
+	scopego()
+	var task = "running" //local scope /block scope :- remains visible until the {} of main()
+	f.Println(task,done)
+	const done =true//valid as scope is different :- local scoped 
+	f.Println("done in main",done)
 }
 
 // func ifElse(){
@@ -78,6 +96,113 @@ func main(){
 // 	*/
 // }
 
-func forLoop(){
+// func forLoop(){
+// 	//for loop 
+// 	for i :=0;i<10;i++{ //1 is init statement with temp var i which only exist inside for loop, 2 is test condition ,3 process statement 
+// 		fmt.Println(i)
+// 	}
+// 	//No while loop in go but we can construct that 
+// 	j:=10
+// 	for j<14{
+// 		fmt.Println(j)
+// 		j++
+// 	}
+// }
 
+// func continueBreakstat(){
+// 	//continue statemenent example 
+// 	for i:=0;i<10;i++{
+// 		if i%2==0{
+// 			fmt.Println(i)
+// 		}else{
+// 			continue //will skip all the functions afterwards and next iteration of loop will start 
+// 			i++
+// 		}
+// 	}
+// 	//
+// 	// Break Statement Example 
+// 	for i:=0;i<10;i+=2{
+// 		if i==8{
+// 			fmt.Println("wohhoo break works!!!")
+// 			break //not end the whole program just came out of process/loop
+// 		}else{
+// 			continue
+// 		}
+// 	}
+// }
+
+// func labelstat(){
+// 	/*Label stat :-  used in break,continue,goto statements
+// 	ILLEGAL to define label statements which is never used 
+// 	They are not block scoped and dont conflict with identifiers that are not labels ,They live in naother space 
+// 	Scope of label is body of fucntion in which its declared and excludes body of any nested function
+// 	Most times labels are used to terminate outer enclosing loops 
+// 	*/
+// 	outer :=19
+// 	_=outer //this will not conflict with identifiers and label with same name hence no error will be seen
+// 	people :=[5]string{"n","b","a","d","e"}
+// 	frien := [2]string{"a","t"}
+// outer://label tells compiler that we care about this place
+// 	for index,name :=range people{//range used to iterate over arrays and return both index and elements one by one from array
+// 		for _,frind := range frien{
+// 			if name==frind{
+// 				fmt.Printf("found friend at %q at %d \n",frind,index)
+// 				break outer //without label it breaks from inner loop only 
+// 				//ith label it will directly comes to outer label statement
+// 			}
+// 		}
+// 	}
+// 	fmt.Println("Next instruction after the break")
+// }
+
+// func gotoSwitchstat(){
+// 	/*GOto :- transfer control to statement with corresponding label within the same fucntion
+// 	It allows us to jump to any label inside the function but break,continue restricted to if,else,for but goto is not */
+// 	i:=0
+// loop: //creates loop like for without using for 
+// 	if i<5{
+// 		fmt.Println(i)
+// 		i++
+// 		goto loop
+// 	}
+// 	//NOTE :- its not allowed to jump to label if new var are introduced 
+// 	// goto todo
+// 	// x:=5
+// 	// todo:
+// 	// 	fmt.Println("ewndjhdb")
+// 	//
+// 	//Goto shuld be used with caution
+// 	//
+// 	/*Switch statements :-  in backend golang converts in nested if only 
+// 	Golang adds break automatically aqafter each case*/
+// 	lang:="golang"
+// 	switch lang{
+// 	case "py":
+// 		fmt.Println("py")
+// 	case "GO","Go","golang":
+// 		fmt.Println("Goood!!!")
+// 	default:
+// 		fmt.Println("HAVE SOME LIFE BRO!!!!!")
+// 	//Cases are evaluated from top to bottom and default is not necessary
+// 	}
+// 	//
+// 	n:=5
+// 	switch{ //ALways true kind of switch statement or can also be written as 
+// 		//switch true{}
+// 	case n%2==0:
+// 		fmt.Println("true")
+// 	case n%2!=0:
+// 		fmt.Println("odd")
+// 	}
+// }
+
+func scopego(){
+	/*Scope :- means visibility,also called as lifetime  of var:- is interval of time during which it exists as program executes
+	Name cant be declared again in same scope ,but can be declared in another scope 
+	3 types of scopes:- 
+	File,Package,Block(local)
+	*/
+	//So we cant import "fmt" again in file but alias can be done import f "fmt"
+	f.Println("Here the value printed for done const will be of package scoped only{no local scope value WILL BE PRINTED HERE}",done)
+	//Unused package scope var doesnt return error 
 }

a9611d7ac753a1fddfea3e6ed8cad72b44a40b4a
PArv Agarwal <parvaggarwal9759@gmail.com>
2025-07-22 02:11:14 +0530

    Completed the challenge of Strings in Golang

---
+++ Go_Notes_Nd_Ex/Coding Challenges /2 Goarrays.go
@@ -0,0 +1,31 @@
+package main
+import(
+	"fmt"
+)
+func main(){
+	/*Exercise 1*/
+	// fmt.Println("Challenge for Arrays in Golang!")
+	// var cities = [2]string{}
+	// fmt.Println(cities)
+	// grades := [3]float64{1.2,2.4}
+	// fmt.Printf("%#v ",grades)
+	// taskDone :=[...]bool{true,false,true,true}
+	// fmt.Println(taskDone)
+	// city:=[...]string{"a","b","c","d","e","f"}
+	// for i:=0;i<len(city);i++{
+	// 	fmt.Println(i,city[i])
+	// }
+	// grades := [...]int{33,44,65,66,87}
+	// for i:=range(grades){
+	// 	fmt.Println(grades[i])
+	// }
+	/*Exercise 2*/
+	// cnt:=0
+	// nums:=[...]int{30,-1,-6,90,-6}
+	// for i:=range(nums){
+	// 	if(nums[i]>0){
+	// 		cnt++
+	// 	}
+	// }
+	// fmt.Println(cnt)
+}
